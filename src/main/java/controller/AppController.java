c434f6b26369151e7c9355699e7fe36e9390aab0
Aleks <feniksizvdv@ukr.net>
2024-04-11 19:11:58 +0300

    Initial commit

---
+++ src/main/java/controller/AppController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import service.AppService;
+import view.AppView;
+
+public class AppController {
+
+    AppView view;
+    AppService service;
+
+    public AppController(AppView view, AppService service) {
+        this.view = view;
+        this.service = service;
+    }
+
+    public void runApp() {
+        service.handleOption(view.getOption());
+    }
+}
